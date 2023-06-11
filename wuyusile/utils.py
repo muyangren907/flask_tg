2f387c360a87431448a97f6f3f09427d191268a3
muyangren907 <30375117+muyangren907@users.noreply.github.com>
2023-06-11 20:31:09 +0000

    hhhj

--- wuyusile/utils.py
+++ wuyusile/utils.py
@@ -22,10 +22,11 @@ from types import GeneratorType
 import logging
 __log__ = logging.getLogger(__name__)
 __log__.info('开始初始化 dasbxueyiSender')
+
+from .extensions import markdown, html
 __log__.info('结束初始化 dasbxueyiSender')
 exit(0)
 
-from .extensions import markdown, html
 from .helpers import add_surrogate, del_surrogate, strip_text
 from .tl import types
 
